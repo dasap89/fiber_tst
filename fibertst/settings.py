7ce7e4a2f07377fb6721289d61dcb2ee052bef25
shelby89 <shelbynagliy@gmail.com>
2016-08-26 09:05:51 +0000

    notes for /reviews/ and children page done

--- fibertst/settings.py
+++ fibertst/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'fibertst/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_FINDERS = DEFAULT_SETTINGS.STATICFILES_FINDERS + [
     'compressor.finders.CompressorFinder',
 ]
