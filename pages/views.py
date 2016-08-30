cc4fffd1b007fdd6811d2ad8559ebaa5b96a000f
csh <dmitrys@webdevelop.pro>
2016-08-30 19:09:37 +0000

    searching functionality added

--- pages/views.py
+++ pages/views.py
@@ -1,11 +1,17 @@
+# -*- coding: utf-8 -*-
+
 from fiber.views import FiberTemplateView
-from fiber.models import Page
+from django.views.generic import ListView
+from django.db.models import Q
+from fiber.models import Page, ContentItem, PageContentItem
 from pages.forms import ContactForm
 from pages.models import ContactModel
 from django.template.loader import get_template
 from django.core.mail import EmailMessage, send_mail
 from django.template import Context
 from django.shortcuts import render, redirect
+
+from pure_pagination.mixins import PaginationMixin
 
 
 class IndexPage(FiberTemplateView):
@@ -64,7 +70,6 @@ def contact_me(request):
             print "I am trying to send email"
             try:
                 email.send()
-                # send_mail('Email from ' + contact_name, contact_message, contact_email, ['shelby_dima@mail.ru'], fail_silently=False)
                 update_saved_form = ContactModel.objects.get(pk=form_id)
                 update_saved_form.sended = True
                 update_saved_form.save()
@@ -77,3 +82,56 @@ def contact_me(request):
     return render(request, 'index.html', {
         'form': form,
     })
+
+class SearchList(PaginationMixin, ListView):
+    paginate_by = 10 #this variable is used for pagination
+    template_name = 'search_results.html'
+    
+    def get_queryset(self):
+                 
+        search = self.request.GET.get('search', None)
+        self.search = search
+        if search:
+            search_in_content = ContentItem.objects.filter(content_html__icontains=search).values('id')
+            search_in_page_content = PageContentItem.objects.filter(content_item__in=search_in_content).values('page_id')
+            objects = Page.objects.filter(
+                Q(id__in=search_in_page_content) | Q(title__icontains=search)
+                )
+        else:
+            objects = Page.objects.all()
+        
+        return objects
+    
+    def get_context_data(self, **kwargs):
+        context = super(SearchList, self).get_context_data(**kwargs)
+        context['search'] = self.search
+        return context
+
+# def search(request):
+#     search = request.GET.get('search', None)
+#     search_in_content = ContentItem.objects.filter(content_html__icontains=search).values('id')
+#     print search_in_content
+#     search_in_page_content = PageContentItem.objects.filter(content_item__in=search_in_content).values('page_id')
+#     print search_in_page_content
+#     search_results = Page.objects.filter(id__in=search_in_page_content)
+#     return render(request, 'search_results.html', {
+#         'search': search,
+#         'search_results': search_results, 
+#     })
+
+
+    """
+    if search_query:
+        search_results = Page.objects.live().search(search_query)
+
+        # Log the query so Wagtail can suggest promoted results
+        Query.get(search_query).add_hit()
+    else:
+        search_results = Page.objects.none()
+
+    # Render template
+    return render(request, 'search_results.html', {
+        'search_query': search_query,
+        'search_results': search_results,
+    })
+    """
